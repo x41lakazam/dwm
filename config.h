9dadff0c9c155d4e7a04a53462ec59f93d4393e5
x41lakazam <alakazamjoined@gmail.com>
2019-10-26 12:35:22 +0300

    Patched useless gaps

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int gappx 	= 10; 		/* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -27,13 +28,13 @@ static const unsigned int alphas[][3]      = {
 
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
+static const char *termcmd[]  = { "st", NULL };
 /** Function to shift the current view to the left/right
  *
  * @param: "arg->i" stores the number of tags to shift right (positive value)
  *          or left (negative value)
  */
-void
-shiftview(const Arg *arg) {
+void shiftview(const Arg *arg) {
 	Arg shifted;
 
 	if(arg->i > 0) // left circular shift
@@ -90,35 +91,39 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
+static const char *firefoxcmd[] = {"firefox"};
 
 #include "movestack.c"
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,			XK_q,		killclient,	{0} },
-	{ MODKEY,                       XK_j,		focusstack,	{.i = +1 } },
-	{ MODKEY,                       XK_k,		focusstack,	{.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_j,		movestack,	{.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_k,		movestack,	{.i = -1 } },
-	{ MODKEY,			XK_space,	zoom,		{0} },
-	{ MODKEY,                       XK_h,		setmfact,	{.f = -0.05} },
-	{ MODKEY,                       XK_l,		setmfact,	{.f = +0.05} },
-	{ MODKEY|ShiftMask,		XK_i,		incnmaster,	{.i = +1 } },
-	{ MODKEY|ShiftMask,		XK_o,		incnmaster,	{.i = -1 } },
-	{ MODKEY,			XK_b,		togglebar,	{0} },
-	{ MODKEY,			XK_t,		setlayout,	{.v = &layouts[0]} },
-	{ MODKEY,			XK_f,		setlayout,	{.v = &layouts[1]} },
-	{ MODKEY,			XK_y,		setlayout,	{.v = &layouts[2]} },
-	{ MODKEY,			XK_u,		setlayout,	{.v = &layouts[3]} },
-	{ MODKEY,			XK_i,		setlayout,	{.v = &layouts[4]} },
-	{ MODKEY,			XK_o,		setlayout,	{.v = &layouts[5]} },
-	{ MODKEY,			XK_Tab,		view,		{0} },
-	{ MODKEY,			XK_backslash,	view,		{0} },
-	{ MODKEY|ShiftMask,		XK_space,	togglefloating,	{0} },
-	/* { MODKEY,                       XK_space,  setlayout,      {0} }, */
-	/* { MODKEY,                       XK_comma,  focusmon,       {.i = -1 } }, */
-	/* { MODKEY,                       XK_period, focusmon,       {.i = +1 } }, */
-	/* { MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } }, */
-	/* { MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } }, */
+	/* modifier                     key        		function        argument */
+	{ MODKEY,    	 	        	XK_p,	 		spawn,          	{.v = dmenucmd} },
+	{ MODKEY|ShiftMask,             XK_Return, 		spawn,          	{.v = termcmd } },
+	{ MODKEY,             			XK_w,	 		spawn,          	{.v = firefoxcmd} },
+	{ MODKEY,						XK_q,			killclient,			{0} },
+	{ MODKEY,                       XK_j,			focusstack,			{.i = +1 } },
+	{ MODKEY,                       XK_k,			focusstack,			{.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_j,			movestack,			{.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,			movestack,			{.i = -1 } },
+	{ MODKEY,						XK_space,		zoom,				{0} },
+	{ MODKEY,                       XK_h,			setmfact,			{.f = -0.05} },
+	{ MODKEY,                       XK_l,			setmfact,			{.f = +0.05} },
+	{ MODKEY|ShiftMask,				XK_i,			incnmaster,			{.i = +1 } },
+	{ MODKEY|ShiftMask,				XK_o,			incnmaster,			{.i = -1 } },
+	{ MODKEY,						XK_b,			togglebar,			{0} },
+	{ MODKEY,						XK_t,			setlayout,			{.v = &layouts[0]} },
+	{ MODKEY,						XK_f,			setlayout,			{.v = &layouts[1]} },
+	{ MODKEY,						XK_y,			setlayout,			{.v = &layouts[2]} },
+	{ MODKEY,						XK_u,			setlayout,			{.v = &layouts[3]} },
+	{ MODKEY,						XK_i,			setlayout,			{.v = &layouts[4]} },
+	{ MODKEY,						XK_o,			setlayout,			{.v = &layouts[5]} },
+	{ MODKEY,						XK_Tab,			view,				{0} },
+	{ MODKEY,						XK_backslash,	view,				{0} },
+	{ MODKEY|ShiftMask,				XK_space,		togglefloating,		{0} },
+	{ MODKEY,                       XK_space,  		setlayout,      	{0} },
+	{ MODKEY,                       XK_comma,  		focusmon,       	{.i = -1 } },
+	{ MODKEY,                       XK_period, 		focusmon,       	{.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_comma,  		tagmon,         	{.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_period, 		tagmon,         	{.i = +1 } },
 	TAGKEYS(			XK_1,			0)
 	TAGKEYS(			XK_2,			1)
 	TAGKEYS(			XK_3,			2)
